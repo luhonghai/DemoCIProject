cbd6ab7ddd1e4f3611d7517c6cfc49734d557be6
Hai Lu <luhonghai@Hais-MacBook-Pro-2.local>
2016-12-28 11:34:11 +0700

    Change function order

--- app/src/main/java/com/mobilehealth/demociproject/MainActivity.java
+++ app/src/main/java/com/mobilehealth/demociproject/MainActivity.java
@@ -44,6 +44,13 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -53,12 +60,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
